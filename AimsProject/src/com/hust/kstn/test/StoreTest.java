17f1d96d06e36f293b4d30f47b6f893e742cc19f
UFOsonzz <tamzuka2006@gmail.com>
2025-11-17 10:21:09 +0700

    Add store and StoreTest

---
+++ AimsProject/src/com/hust/kstn/test/StoreTest.java
@@ -0,0 +1,26 @@
+package com.hust.kstn.test;
+
+import com.hust.kstn.models.Store;
+import com.hust.kstn.models.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("DoMixi funny moments", 
+        "Comedy", "DoMixi", 360, 36.36);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Anhtoi xin loi khan gia", 
+        "Tragedy", "Anhtoi", 63, 0.63);
+
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.removeDVD(dvd2);
+        store.removeDVD(dvd1);
+        store.removeDVD(dvd1);
+        for (int i = 0; i < 101; i++) {
+            String randomTitle = Integer.toString(i);
+            DigitalVideoDisc newDisc = new DigitalVideoDisc(randomTitle);
+            store.addDVD(newDisc);
+        }
+
+    }
+}
